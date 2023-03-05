910855afba7eea0dbca69490ee91a1b7b152fb4a
Ukinion <114142826+Ukinion@users.noreply.github.com>
2023-03-05 19:48:22 +0600

    [lab1-StackCalculator] add exception
    added and implemented exceptions 
    throw when divider = 0

---
+++ lab1StackCalculator/src/exceptions/DivisionByZeroException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class DivisionByZeroException extends ArithmeticException
+{
+    public DivisionByZeroException()
+    { super ("ArithmeticError: division by zero\n\t\t"); }
+}
