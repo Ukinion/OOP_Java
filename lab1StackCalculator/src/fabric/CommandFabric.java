6581af464696f09d9c7f86fa521569f84c943b38
Ukinion <114142826+Ukinion@users.noreply.github.com>
2023-03-05 19:24:14 +0600

    [lab1-StackCalculator] rewrite logger
    rewritten logger
    changed local variables name

--- lab1StackCalculator/src/fabric/CommandFabric.java
+++ lab1StackCalculator/src/fabric/CommandFabric.java
@@ -14,45 +14,58 @@ public class CommandFabric
     private Logger _logger;
     private Scanner _cfgScanner;
 
-    public CommandFabric(String configName)
+    private final static String CONFIG_DELIMITER = "=";
+    private final static int COMMAND_USER_NAME = 0;
+    private final static int COMMAND_CLASS_NAME = 1;
+
+    public CommandFabric(String configName) throws FileNotFoundException
     {
         _logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        try 
+        try
         { _cfgScanner = new Scanner(new File(configName)); }
         catch (FileNotFoundException ex)
-        { _logger.severe("Scanner could not open config file.\n\t\tHave to start aborting... \n\t\t" + ex.getMessage()); }
+        {
+            _logger.severe("Scanner could not open config file!\n\t\tHave to start aborting...\n\t\t" + ex.getMessage());
+            throw ex;
+        }
     }
 
-    public HashMap<String, Command> createCommandMap()
+    public HashMap<String, Command> createCommandMap() throws Exception
     {
-        HashMap<String, Command> commandMap = new HashMap<String, Command>();
+        HashMap<String, Command> commandMap = new HashMap<>();
 
         int cntLine = 0;
         String curLine;
-        String[] keyAndValue;
+        String[] configData;
         Class<?> commandClass;
 
         while (_cfgScanner.hasNextLine())
         {
             ++cntLine;
             curLine = _cfgScanner.nextLine();
-            keyAndValue = curLine.split("=");
+            configData = curLine.split(CONFIG_DELIMITER);
 
             try
             {
-                commandClass = Class.forName(keyAndValue[1]);
-                commandMap.put(keyAndValue[0], (Command) commandClass.getConstructor().newInstance());
+                commandClass = Class.forName(configData[COMMAND_CLASS_NAME]);
+                commandMap.put(configData[COMMAND_USER_NAME], (Command) commandClass.getConstructor().newInstance());
             }
             catch (ArrayIndexOutOfBoundsException ex)
-            { _logger.warning("Incorrect command line: №" + cntLine + "\n\t\t" + "Trying to ignore...\n\t\t" + ex.getMessage()); }
+            {
+                _logger.severe("Incorrect command line: №" + cntLine + "\n\t\t" + "Stop processing...\n\t\t" + ex.getMessage());
+                throw ex;
+            }
             catch (ClassNotFoundException ex)
-            { _logger.severe("Config contains unsupported command.\n\t\t Error!\n\t\t" + ex.getMessage()); }
+            {
+                _logger.severe("Config contains unsupported command.\n\t\t Error!\n\t\t" + ex.getMessage());
+                throw ex;
+            }
             catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | InvocationTargetException ex)
-            { _logger.severe("System detected internal error.\n\t\t" + ex.getMessage()); }
+            {
+                _logger.severe("System detected internal error.\n\t\t" + ex.getMessage());
+                throw ex;
+            }
         }
-
         return commandMap;
     }
 }
-
-
