d1940297fba7ecb92c917d50dc7cd1b17d4a0e41
Ukinion <114142826+Ukinion@users.noreply.github.com>
2023-03-12 16:00:15 +0600

    [lab1-StackCalculator] increase logging, exception
    added new exceptions
    calculator work is logged

--- lab1StackCalculator/src/calculator/Calculator.java
+++ lab1StackCalculator/src/calculator/Calculator.java
@@ -5,6 +5,7 @@ import fabric.CommandFabric;
 import fabric.command.Command;
 import context.ProgramContext;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Stack;
@@ -14,8 +15,8 @@ import java.util.Scanner;
 
 public class Calculator
 {
-    private Logger _logger;
-    private HashMap<String, Command> _commandMap;
+    private final Logger _logger;
+    private final HashMap<String, Command> _commandMap;
 
     private final static String COMMAND_LINE_DELIMITER = " ";
     private final static int COMMAND = 0;
@@ -24,16 +25,24 @@ public class Calculator
     public final static String VARIABLE_CONTEXT = "variableContext";
     public final static String COMMAND_INFO_CONTEXT = "commandContext";
 
-    public Calculator(String configName) throws Exception
+    public Calculator(String configName) throws RuntimeException
     {
-        _logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        CommandFabric fabric = new CommandFabric(configName);
-        _commandMap = fabric.createCommandMap();
+        try
+        {
+            _logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+            _logger.info("Calculator is launching");
+            CommandFabric fabric = new CommandFabric(configName);
+            _commandMap = fabric.createCommandMap();
+            _logger.info("Calculator is launched\n");
+        }
+        catch (RuntimeException | ReflectiveOperationException | IOException ex)
+        { throw new FabricInternalErrorException(); }
     }
 
     public void calculate(InputStream inputStream) throws RuntimeException
     {
-        _logger.info("Starting calculation...\n");
+        _logger.info("Start Calculating\n");
+
         Scanner commandFile = new Scanner(inputStream);
         HashMap<String, Double> variableMap = new HashMap<String, Double>();
         Stack<String> stack = new Stack<>();
@@ -54,29 +63,41 @@ public class Calculator
             commandAndArguments = curLine.split(COMMAND_LINE_DELIMITER);
 
             if (!_commandMap.containsKey(commandAndArguments[COMMAND]))
-            { throw new UnsupportedCommandException(); }
+            { throw new UnsupportedCommandException(commandAndArguments[COMMAND]); }
 
             context.addContext(COMMAND_INFO_CONTEXT, commandAndArguments);
             command = _commandMap.get(commandAndArguments[COMMAND]);
 
             try
-            { command.execute(context); }
+            {
+                _logger.info("Executing command: " + command.getClass().getName());
+                command.execute(context);
+                _logger.info("End executing\n");
+            }
             catch (InvalidCommandArgumentListException ex)
             {
-                _logger.severe("Not enough arguments to execute command\n\t\t" + ex.getMessage());
-                throw new RuntimeException("Execution Error!");
+                _logger.severe(ex.getMessage() + "Change argument list\n");
+                throw new ExecutionCommandErrorException();
             }
             catch (NotEnoughOperandsException ex)
             {
-                _logger.severe("Aborting command execution...\n\t\t" + ex.getMessage());
-                throw new RuntimeException("Execution Error!");
+                _logger.severe(ex.getMessage() + "Aborting command execution...\n");
+                throw new ExecutionCommandErrorException();
+            }
+            catch (UndefinedExecutionContextException ex)
+            {
+                _logger.severe(ex.getMessage() + "No further execution without context");
+                throw new ExecutionCommandErrorException();
             }
             catch (DivisionByZeroException | SquareNegativeNumberException ex)
             {
                 _logger.severe(ex.getMessage());
-                throw new RuntimeException("Execution Error!");
+                throw new ExecutionCommandErrorException();
             }
         }
+
+        _logger.info("Ending calculating\n");
+        _logger.info("Work is done\n");
     }
 }
 
